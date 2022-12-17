06521fdb8d49c217193f5adf7bab79fbcde4faef
Shawn Wagner <shawnw.mobile@gmail.com>
2022-12-17 05:02:15 -0800

    More CI and doc tweaks

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection 'multi)
 (define deps '("base" "extra-srfi-libs"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("data/scribblings/priority-queue.scrbl" ())))
+(define scribblings '(("data/scribblings/priority-queue.scrbl" () ("Data Structures"))))
 (define pkg-desc "Priority Queues")
 (define version "0.0")
 (define pkg-authors '(shawnw))
